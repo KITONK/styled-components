f55cedc9e0157a7c0fc6f9d46120a4ffe1fda27f
KITONK <kiyj585@gmail.com>
2022-01-26 23:46:53 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from './components/Button';
+import Console from './components/Console';
+import Flex from './components/Flex';
+import Title from './components/Title';
+
+const AppWrapper = styled.div`
+width:100%;
+min-height: 100vh;
+padding: 2rem;
+background: black;
+`
+
+const App = () => {
+  return (
+    <AppWrapper>
+      <Flex justify="center">
+        <Title>Console cmd 2022. Zipkito</Title>
+      </Flex>
+      <Flex direction="column" margin={"10px 0"}>
+        <Console/>
+        <Button outline align="flex-end">Send</Button>
+      </Flex>
+    </AppWrapper>
+  );
+}
+
+export default App;
